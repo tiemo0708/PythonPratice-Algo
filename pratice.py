c1747d9ce8d47b47017e4e901d90d761711778ff
tiemo0708 <tiemo0708@naver.com>
2023-01-29 00:17:07 +0000

    23/1/29

--- pratice.py
+++ pratice.py
@@ -714,11 +714,20 @@ class Unit:
         self.name = name #멤버변수
         self.hp = hp
         self.speed = speed
+        print("{0} 유닛이 생성되었습니다.".format(name))
     
     def move(self, location):
         print("[지상 유닛 이동]")
         print("{0} : {1} 방향으로 이동합니다. [속도 {2}]"\
             .format(self.name, location, self.speed))
+
+    def damaged(self, damage):
+        print("{0} : {1} 데미지를 입었습니다.".format(self.name, damage))
+        self.hp -= damage
+        print("{0}: 현재 체력은 {1} 입니다.".format(self.name, self.hp))
+        if self.hp <= 0:
+            print("{0} : 파괴되었습니다.".format(self.name))
+
       
 #공격유닛
 class AttackUnit(Unit): #(상속받을 클래스)
@@ -730,12 +739,6 @@ class AttackUnit(Unit): #(상속받을 클래스)
         print("{0} : {1} 방향으로 적군을 공격 합니다. [공격력 {2}]"\
             .format(self.name, loacation, self.damage))
 
-    def damaged(self, damage):
-        print("{0} : {1} 데미지를 입었습니다.".format(self.name, damage))
-        self.hp -= damage
-        print("{0}: 현재 체력은 {1} 입니다.".format(self.name, self.hp))
-        if self.hp <= 0:
-            print("{0} : 파괴되었습니다.".format(self.name))
 
 #날 수 있는 기능을 가진 클래스
 class flyable:
@@ -746,7 +749,7 @@ class flyable:
         print("{0} : {1} 방향으로 날아갑니다. [속도 {2}]"\
             .format(name, location, self.flying_speed))
 
-# 공중 공격 유낫 클래스
+# 공중 공격 유닛 클래스
 class FlyableAttackUnit(AttackUnit, flyable): #다중상속
     def __init__(self, name, hp, damage, flying_speed):
             AttackUnit.__init__(self, name, hp, 0, damage)
@@ -757,14 +760,56 @@ class FlyableAttackUnit(AttackUnit, flyable): #다중상속
         print("[공중 유닛 이동]")
         self.fly(self.name, location)
 
+class Marine(AttackUnit):
+    def __init__(self):
+         AttackUnit.__init__(self, "마린", 40, 1, 5)
 
-#건물
-class BuildingUnit(Unit):
-    def __init__(self, name, hp, location):
-        # pass #아무것도 안하고 일단 넘어감
-        # Unit.__init__(self, name, hp, 0)
-        super().__init__(name, hp, speed)#(). self 없이
-        self.location = location
+    #스팀팩 : 일정 시간 동안 이동 및 공격 속도를 증가, 체력 10 감소
+
+    def stimpack(self):
+        if self.hp > 10:
+            self.hp-=10
+            print("{0}: 스팀팩을 사용합니다. (HP 10 감소".format(self.name))
+        else:
+            print("{0} : 체력이 부족하여 스팀팩을 사용하지 않습니다".format(self.name))
+
+
+#탱크
+class Tank(AttackUnit):
+    #시즈모드: 탱크를 지상에 고정시켜, 더 높은 파워로 공격 가능, 이동불가
+    seize_development = False # 시즈모드 개발여부
+    
+    def __init__(self):
+        AttackUnit.__init__(self, "탱크", 150, 1, 35)
+        self.seize_mode = False
+
+    def set_seize_mode(self):
+        if Tank.seize_development == False:
+            return
+        
+        #현재 시즈모드가 아닐때 -> 시즈모드
+        if self.seize_mode == False:
+            print("{0}: 시즈모드로 전환합니다.".format(self.name))
+            self.damage *= 2
+            self.seize_mode = True
+
+        #현재 시즈모드 일때 -> 시즈모드 해제
+        else:
+            print("{0}: 시즈모드로 전환합니다.".format(self.name))
+            self.damage /= 2
+            self.seize_mode = False
+
+#레이스
+
+
+
+# #건물
+# class BuildingUnit(Unit):
+#     def __init__(self, name, hp, location):
+#         # pass #아무것도 안하고 일단 넘어감
+#         # Unit.__init__(self, name, hp, 0)
+#         super().__init__(name, hp, speed)#(). self 없이
+#         self.location = location
         
 
 
