b1cb7245cfb759013b16f119b83f83bc3b806b2b
tiemo0708 <tiemo0708@naver.com>
2023-04-26 14:43:14 +0000

    3

--- Algo/Greedy/3.py
+++ Algo/Greedy/3.py
@@ -1,14 +1,26 @@
-#곱하기 혹은 더하기
+# #곱하기 혹은 더하기
 
-data= list(map(int,list(input())))
-result = data[0];
+# data= list(map(int,list(input())))
+# result = data[0];
+# for i in range(1, len(data)):
+#     num = data[i]
+#     if num <= 1 or result <= 1:
+#         result += num
+#     else:
+#         result*= num
+
+# print(result)
+
+data = input()
+
+#첫 번쨰 문자를 숫자로 변경하여 대입
+result = int(data[0])
+
 for i in range(1, len(data)):
-    num = data[i]
-    if num <= 1 or result <= 1:
-        result += num
+    #두 수중에서 하나라도 '0' 혹은 '1'인 경우, 곱하기보다는 더하기 수행
+    num = int(data[i])
+    if num<=1 or result <=1:
+        result+=num
     else:
-        result*= num
-
+        result*=num
 print(result)
-
-    
