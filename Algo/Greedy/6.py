a5c0704b126a19e34c4921c5a82a40f0cfb51fc7
tiemo0708 <tiemo0708@naver.com>
2023-05-28 01:54:03 +0000

    시각문제

---
+++ Algo/Greedy/6.py
@@ -0,0 +1,16 @@
+#시각 문제 - 완전탐색
+
+# H 입력 받기
+h = int(input())
+
+count = 0
+for i in range(h+1):
+    for j in range(60):
+        for k in range(60):
+            #매 시각 안에 '3'이 들어 있으면 카운트 증가
+            if '3' in str(i)+str(j)+str(k):
+                count+=1
+
+print(count)
+
+#입력 5 출력 11475
